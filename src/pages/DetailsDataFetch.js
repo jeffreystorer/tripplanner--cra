c9de1aecdf869492f282e2733e52f3d69e0d1d93
jeffreystorer <jeffrey.storer@gmail.com>
2022-05-19 14:04:53 -0400

    5/19 before refactor navigation

--- src/pages/DetailsDataFetch.js
+++ src/pages/DetailsDataFetch.js
@@ -8,15 +8,33 @@ import { DetailsPage } from 'pages';
 import * as state from 'store';
 
 export default function DetailsDataFetchPage({ page }) {
+  console.log(
+    '🚀 ~ file: DetailsDataFetch.js ~ line 11 ~ DetailsDataFetchPage ~ page',
+    page
+  );
   const userId = useRecoilValue(state.userId);
   const currentTripKey = useRecoilValue(state.currentTripKey);
   const app = initializeApp(firebaseConfig);
   const db = getDatabase(app);
-  const dbRef = ref(db, `/${userId}/${currentTripKey}/details/${page}/`);
+  const path = `/${userId}/${currentTripKey}/details/${page}/`;
+  console.log(
+    '🚀 ~ file: DetailsDataFetch.js ~ line 17 ~ DetailsDataFetchPage ~ path',
+    path
+  );
+  const dbRef = ref(db, path);
   const [snapshots, loading, error] = useList(dbRef);
 
   if (error) return <strong>Error: {error}</strong>;
   if (loading) return <Loading />;
-
+  if (snapshots) {
+    console.log(
+      '🚀 ~ file: DetailsDataFetch.js ~ line 24 ~ DetailsDataFetchPage ~ snapshots[0].val()',
+      snapshots[0].val()
+    );
+    console.log(
+      '🚀 ~ file: DetailsDataFetch.js ~ line 24 ~ DetailsDataFetchPage ~ snapshots[0].key',
+      snapshots[0].key
+    );
+  }
   return <>{snapshots && <DetailsPage snapshots={snapshots} page={page} />}</>;
 }
