00aeab350b693122643397123bb4012c1630b518
jeffreystorer <jeffrey.storer@gmail.com>
2022-05-23 14:45:20 -0400

    5/23 delete console.logs

--- src/pages/DetailsGatewayPage.js
+++ src/pages/DetailsGatewayPage.js
@@ -8,27 +8,15 @@ import { DetailsPage } from 'pages';
 import * as state from 'store';
 
 export default function DetailsGatewayPage({ page }) {
-  console.log(
-    '🚀 ~ file: DetailsGatewayPage.js ~ line 11 ~ DetailsGatewayPage ~ page ',
-    page
-  );
   const userId = useRecoilValue(state.userId);
   const currentTripKey = useRecoilValue(state.currentTripKey);
   const app = initializeApp(firebaseConfig);
   const db = getDatabase(app);
   const path = `/${userId}/${currentTripKey}/details/${page}/`;
   const dbRef = ref(db, path);
-  console.log(
-    '🚀 ~ file: DetailsGatewayPage.js ~ line 21 ~ DetailsGatewayPage ~ path',
-    path
-  );
   const [snapshots, loading, error] = useList(dbRef);
 
   if (error) return <strong>Error: {error}</strong>;
   if (loading) return <Loading />;
-  console.log(
-    '🚀 ~ file: DetailsGatewayPage.js ~ line 26 ~ DetailsGatewayPage ~ snapshots[0].val()',
-    snapshots[0].val()
-  );
   return <>{snapshots && <DetailsPage snapshots={snapshots} page={page} />}</>;
 }
